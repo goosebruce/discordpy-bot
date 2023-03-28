8d5d5a28b65347e35a1d57350e96b91f7be58653
goosebruce <nklockhart2@gmail.com>
2023-03-27 19:03:48 -0600

    log

--- events/pro_groups.py
+++ events/pro_groups.py
@@ -35,6 +35,7 @@ async def add_pro_group_channel(member, role) -> None:
 
 async def handle_pro_role_change(before, after) -> None:
     print("member change event triggered")
+    print(before.roles)
     print(after.roles)
     pro_role = discord.utils.get(after.roles, name="Pro")
     if pro_role is None:
