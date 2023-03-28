2c05bdcf81ac30d2f10a69889772ab968b2c3056
goosebruce <nklockhart2@gmail.com>
2023-03-28 09:43:25 -0600

    handle remove

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import discord
 from discord.ext import commands
+
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+
 from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -80,11 +83,17 @@ async def on_member_update(before, after):
     basic_role = discord.utils.get(after.guild.roles, name="Basic")
     pro_role = discord.utils.get(after.guild.roles, name="Pro")
     private_role = discord.utils.get(after.guild.roles, name="Private")
-    if basic_role in after.roles and basic_role not in before.roles:
+    if (basic_role in after.roles and basic_role not in before.roles) or (
+        basic_role in before.roles and basic_role not in after.roles
+    ):
         await basic_groups.handle_basic_role_change(before, after)
-    elif pro_role in after.roles and pro_role not in before.roles:
+    elif (pro_role in after.roles and pro_role not in before.roles) or (
+        pro_role in before.roles and pro_role not in after.roles
+    ):
         await pro_groups.handle_pro_role_change(before, after)
-    elif private_role in after.roles and private_role not in before.roles:
+    elif (private_role in after.roles and private_role not in before.roles) or (
+        private_role in before.roles and private_role not in after.roles
+    ):
         await private_groups.handle_private_role_change(before, after)
     else:
         print("No notable role changed")
@@ -166,3 +175,13 @@ async def sendEmbed(request: Request):
     embed.set_footer(text="LeadBot by UpSource Labs")
     await channel.send(embed=embed)
     return {"status": "ok"}
+
+
+@app.get("/")
+async def pulse():
+    html = """<html>
+    <head>
+    <p>online</p>
+    </head>
+    </html>"""
+    return HTMLResponse(content=html, status_code=200)
