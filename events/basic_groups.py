efc1af19c1a1c474c90c025bd669c9931400e4c0
goosebruce <nklockhart2@gmail.com>
2023-04-12 16:51:00 -0600

    updates

--- events/basic_groups.py
+++ events/basic_groups.py
@@ -1,5 +1,7 @@
 import json
 import discord
+import aiohttp
+import os
 
 with open("config.json") as f:
     config = json.load(f)
@@ -31,6 +33,19 @@ async def add_basic_group_channel(member, role) -> None:
         print(f"Channel already exists:{channel_name}")
 
 
+async def fetch_lead_group(api_key, user_id):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://app.upsourcelabs.com/version-test/api/1.1/wf/getleadgroup?discord_id={user_id}"
+        headers = {"Authorization": f"Bearer {api_key}"}
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data["lead_group"]
+            else:
+                print(f"Error fetching lead group: {response.status}")
+                return None
+
+
 async def handle_basic_role_change(before, after) -> None:
     print("member change event triggered")
     print(before.roles)
@@ -43,39 +58,31 @@ async def handle_basic_role_change(before, after) -> None:
                 print(f"Removing {role} from {after}")
                 await after.remove_roles(role)
         return
+
     if basic_role not in before.roles:
         # basic role was added, assign member to basic group
-        if config["basic_role_pre"] not in [r.name for r in after.roles]:
-            print("Member does not have basic group role")
-            # add a loop at all groups that start with the basic role pre and count members in each group, if any are less than 5, assign the role to the member
-            roles = [
-                r
-                for r in after.guild.roles
-                if r.name.startswith(config["basic_role_pre"])
-            ]
-            need_new_role = False
-            for role in roles:
-                if len(role.members) < config["max_basic_members_per_group"]:
-                    await after.add_roles(role)
-                    print(f"Added {role} to")
-                    await add_basic_group_channel(after, role)
-                    return
-                else:
-                    need_new_role = True
-            if need_new_role:
-                group_num = len(roles) + 1
-                new_role = await after.guild.create_role(
-                    name=f"{config['basic_role_pre']}{group_num}",
-                    color=discord.Color.random(),
+        # Replace this block with the Bubble API call
+        api_key = os.environ["bubble_api_key"]
+        user_id = str(after.id)
+        lead_group = await fetch_lead_group(api_key, user_id)
+
+        if lead_group:
+            # Use the lead_group name retrieved from the Bubble API
+            role = discord.utils.get(after.guild.roles, name=lead_group)
+            if not role:
+                # If the role doesn't exist, create it
+                role = await after.guild.create_role(
+                    name=lead_group, color=discord.Color.random()
                 )
-                await new_role.edit(
+                await role.edit(
                     position=after.guild.get_role(after.guild.id).position + 1
                 )
-                await after.add_roles(new_role)
-                await add_basic_group_channel(after, new_role)
+                print(f"New role '{lead_group}' created")
 
+            await after.add_roles(role)
+            await add_basic_group_channel(after, role)
         else:
-            print("Member already has basic group role")
+            print("Unable to fetch lead group from Bubble API")
 
     else:
         print("Member already has basic role")
