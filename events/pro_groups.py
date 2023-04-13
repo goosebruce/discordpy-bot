04f3261f16ca7ae5005ae7488fb48900facb2dfc
goosebruce <nklockhart2@gmail.com>
2023-04-12 19:45:57 -0600

    pro role

--- events/pro_groups.py
+++ events/pro_groups.py
@@ -1,5 +1,7 @@
 import json
 import discord
+import aiohttp
+import os
 
 with open("config.json") as f:
     config = json.load(f)
@@ -31,51 +33,58 @@ async def add_pro_group_channel(member, role) -> None:
         print(f"Channel already exists:{channel_name}")
 
 
+async def fetch_lead_group(api_key, user_id):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://app.upsourcelabs.com/version-test/api/1.1/wf/getleadgroup?discord_id={user_id}"
+        headers = {"Authorization": f"Bearer {api_key}"}
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                print(data["response"]["lead group"])
+                return data["response"]["lead group"]
+
+            else:
+                print(f"Error fetching lead group: {response.status}")
+                return None
+
+
 async def handle_pro_role_change(before, after) -> None:
     print("member change event triggered")
     print(before.roles)
     print(after.roles)
     pro_role = discord.utils.get(after.roles, name="Pro")
     if pro_role is None:
-        # Pro role was removed, remove all pro group roles from member
+        # pro role was removed, remove all pro group roles from member
         for role in after.roles:
             if role.name.startswith(config["pro_role_pre"]):
                 print(f"Removing {role} from {after}")
                 await after.remove_roles(role)
         return
+
     if pro_role not in before.roles:
-        # Pro role was added, assign member to pro group
-        if config["pro_role_pre"] not in [r.name for r in after.roles]:
-            print("Member does not have pro group role")
-            # add a loop at all groups that start with the pro role pre and count members in each group, if any are less than 5, assign the role to the member
-            roles = [
-                r
-                for r in after.guild.roles
-                if r.name.startswith(config["pro_role_pre"])
-            ]
-            need_new_role = False
-            for role in roles:
-                if len(role.members) < config["max_pro_members_per_group"]:
-                    await after.add_roles(role)
-                    print(f"Added {role} to")
-                    await add_pro_group_channel(after, role)
-                    return
-                else:
-                    need_new_role = True
-            if need_new_role:
-                pro_group_num = len(roles) + 1
-                new_role = await after.guild.create_role(
-                    name=f"{config['pro_role_pre']}{pro_group_num}",
-                    color=discord.Color.random(),
+        # pro role was added, assign member to pro group
+        # Replace this block with the Bubble API call
+        api_key = os.environ["bubble_api_key"]
+        user_id = str(after.id)
+        lead_group = await fetch_lead_group(api_key, user_id)
+
+        if lead_group:
+            # Use the lead_group name retrieved from the Bubble API
+            role = discord.utils.get(after.guild.roles, name=lead_group)
+            if not role:
+                # If the role doesn't exist, create it
+                role = await after.guild.create_role(
+                    name=lead_group, color=discord.Color.random()
                 )
-                await new_role.edit(
+                await role.edit(
                     position=after.guild.get_role(after.guild.id).position + 1
                 )
-                await after.add_roles(new_role)
-                await add_pro_group_channel(after, new_role)
+                print(f"New role '{lead_group}' created")
 
+            await after.add_roles(role)
+            await add_pro_group_channel(after, role)
         else:
-            print("Member already has pro group role")
+            print("Unable to fetch lead group from Bubble API")
 
     else:
         print("Member already has pro role")
